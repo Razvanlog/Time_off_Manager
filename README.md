9b553d1fed5380e1be46ee7a561ae98a96e229ae
Razvanlog <razvan.diaconescu2004@gmail.com>
2025-04-07 15:40:07 +0300

    Third commit, added swagger

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Time_off_Manager
+How to run backend:
+	-while in the backend directory type: mvn spring-boot: run
+How to run frontend:
+	-while in the frontend directory type: ng serve
+
+After both parts are running, go to localhost:4200.
+To access swagger part, enter localhost:8080/swagger-ui/index.html
--- Time_off_Manager_BackEnd/src/main/java/com/Turtles/Time_off_Manager_BackEnd/Mycontroller.java
+++ Time_off_Manager_BackEnd/src/main/java/com/Turtles/Time_off_Manager_BackEnd/Mycontroller.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins="http://localhost:4200")
 public class Mycontroller {
     @GetMapping("/hello")
+    @Operation(summary="Hello from spring boot")
+    @ApiResponse(responseCode="240", description = "response sent succesfuly")
     public String sayHello(){
          return "Greetings from Spring Boot!";
     }
